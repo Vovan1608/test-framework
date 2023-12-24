b2564008f2473b92cb0aa6d069c085e3da448e8e
Vova Zenin <vovan1608@gmail.com>
2023-12-24 19:06:57 +0200

    add log4j

--- src/test/java/framework/test/SignInNotValidPasswordTest.java
+++ src/test/java/framework/test/SignInNotValidPasswordTest.java
@@ -8,10 +8,14 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.time.Duration;
 import java.util.stream.Stream;
 
 public class SignInNotValidPasswordTest extends TestRunner{
+    private static final Logger logger = LogManager.getLogger(SignInNotValidPasswordTest.class);
     private static Stream<Arguments> providePasswords() {
         return Stream.of(
                 Arguments.of(NotValidPasswordRepository.getNotValidPassword1()),
@@ -24,12 +28,13 @@ public class SignInNotValidPasswordTest extends TestRunner{
     @ParameterizedTest(name = "{index} => credential={0}")
     @MethodSource("providePasswords")
     public void checkEmail(Credential credential) {
-        int IMPLICIT_WAITING = 8;
+        logger.info("Logging");
+        int IMPLICIT_WAITING_SEC = 8;
         String incorrectPasswordMessage = "Bad email or password";
 
         guestFunctions.signIn(credential);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAITING));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAITING_SEC));
         String actual = driver.findElement(By.xpath("//div[@class='alert-general-error ng-star-inserted']")).getText().trim();
 
         Assertions.assertEquals(actual, incorrectPasswordMessage);
