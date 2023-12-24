31f73edc08b94cd4c0b11dafc4cdd70778ece7b0
Vova Zenin <vovan1608@gmail.com>
2023-12-24 20:19:40 +0200

    add dependency

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,17 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+
+//        System.out.println( "Hello World!" );
+        logger.info("Hello World");
+
     }
 }
